a7585a851b6e2847324f388b4b5d971531d0f027
tjom Emmanuel <tjomemmanuel@gmail.com>
2022-08-12 00:20:11 +0100

    Composant Stateless et Statefull

---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function Greet() {
+    return ( 
+        <h2>Helle great world</h2>
+     );
+} 
+
+//export const Greet = () => <h3>Nous sommes hello world</h3>
+export const Greeting = () => <h3>I thnak God for my life </h3>
+
+//export default Greet;
---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h3>Welcome Mr Emmanuel</h3>
+        );
+    }
+}
+ 
+export default Welcome;
