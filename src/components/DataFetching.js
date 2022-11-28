4a1f354247b244d141a027206978a99cb26d1571
fabuloussTeam <tjomemmanuel@gmail.com>
2022-11-28 13:22:17 +0100

    Fecthing one post with Axios

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -3,26 +3,30 @@ import React, { useState, useEffect } from 'react';
 
 function DataFetching() {
 
-    const [posts, setPosts] = useState([])
-
+    const [post, setPost] = useState({})
+    const [id, setId] = useState(1)
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then(res => {
             console.log(res);
-            setPosts(res.data)
+            setPost(res.data)
         }).catch(err => {
             console.log(err);
         })
-    }, [] )
+    }, [id] )
 
     return ( 
         <div>
             {
-            posts.map(post => <ul key={post.id}>
+                <React.Fragment>
+                    <input type='text' value={id} onChange={(e) => setId(e.target.value)} />
+                <h2>{post.title}</h2>
+                </React.Fragment>
+             /** posts.map(post => <ul titlekey={post.id}>
                  <li>{post.userId}</li> 
                  <li>{post.title}</li> 
                  <li>{post.body}</li> 
-            </ul>)
+            </ul>) */
             }
         </div>
      );
